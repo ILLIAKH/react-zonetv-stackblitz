33fc7b9fcd2e307c557784b6569b54241de3aa4c
ILLIAKH <illia.khomenko@gmail.com>
2021-11-29 23:40:57 -0500

    added functionality to enable/disable scroll once user enters the carousel view

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 //import data
 import * as data from './data/data.json';
 
@@ -9,29 +9,28 @@ import Hero from './components/Hero/Hero';
 import './style.css';
 
 export default function App() {
-
   const [filmData, setFilmData] = useState(data.results);
   const [heroSelection, setHeroSelection] = useState(filmData[0]);
 
-//on app init set filmData with the data from file
-useEffect(()=>{
+  //on app init set filmData with the data from file
+  //useEffect(()=>{
   // setFilmData(data.results);
   // setHeroSelection(filmData[0]);
-  console.log(filmData);
-})
+  //console.log(filmData);
+  //})
 
-function updateHero(curr_selection){
-  setHeroSelection(curr_selection);
-}
+  function updateHero(curr_selection) {
+    setHeroSelection(curr_selection);
+  }
 
-return (
-  <div className="App">
-    {/* Hero and Carousel are sibling components so I made sure that they can
+  return (
+    <div className="App">
+      {/* Hero and Carousel are sibling components so I made sure that they can
       communicate data with each other through APP in this case. 
       Normally it would be a parent component included in main app
     */}
-    <Hero heroSelection={heroSelection} />
-    <Carousel updateHero={updateHero} filmData={filmData} title="Top 20"/>
-  </div>
-);
+      <Hero heroSelection={heroSelection} />
+      <Carousel updateHero={updateHero} filmData={filmData} title="Top 20" />
+    </div>
+  );
 }
--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -8,14 +8,26 @@ function Carousel(props) {
 
   //will allow user to scroll horizontaly using mouse wheel
   const onWheel = (e) => {
-    // e.preventDefault();
+    e.preventDefault;
+    //When a ref is passed to an element in render, a reference to the node becomes accessible at the current attribute of the ref.
     const container = ref.current;
     const containerScrollPosition = ref.current.scrollLeft;
     // console.log(containerScrollPosition, e.deltaY);
+
+    //deltaY returns + when scroll up and - when scroll down
     container.scrollTo({
       top: 0,
       left: containerScrollPosition + e.deltaY,
     });
+  };
+
+  //disables scroll once user enters the carousel with their mouse
+  const disableScroll = () => {
+    document.body.classList.add('no-scroll');
+  };
+  // enables scroll on mouseLeave
+  const enableScroll = () => {
+    document.body.classList.remove('no-scroll');
   };
 
   //will fill carousel with the film items
@@ -40,7 +52,13 @@ function Carousel(props) {
       <div className="carousel-title">
         <h2>{props.title}</h2>
       </div>
-      <div className="Carousel" ref={ref} onWheel={onWheel}>
+      <div
+        className="Carousel"
+        ref={ref}
+        onWheel={onWheel}
+        onMouseEnter={disableScroll}
+        onMouseLeave={enableScroll}
+      >
         {/* <button onClick={()=> scroll(width/5 * 5)}>to tright</button> */}
         {filmItems}
       </div>
